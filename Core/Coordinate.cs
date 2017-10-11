0f6a69d2afae7e65011a442d9858d4e580291b18
mGuv <mattmcg.gamedev@gmail.com>
2017-10-11 20:54:30 +0100

    Add comments for coordinate

--- Core/Coordinate.cs
+++ Core/Coordinate.cs
@@ -1,10 +1,25 @@
 ﻿namespace Core
 {
+    /// <summary>
+    /// Represents a position in 2D Space
+    /// </summary>
     public struct Coordinate
     {
+        /// <summary>
+        /// The X coordinate of the position
+        /// </summary>
         public int X;
+        
+        /// <summary>
+        /// The Y coordinate of the position
+        /// </summary>
         public int Y;
 
+        /// <summary>
+        /// Creates a new Coordinate with the given positions
+        /// </summary>
+        /// <param name="x">The position in the X axis</param>
+        /// <param name="y">The position in the Y axis</param>
         public Coordinate(int x, int y)
         {
             this.X = x;
