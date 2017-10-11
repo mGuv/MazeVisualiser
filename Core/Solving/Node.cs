905761871928da65edea5845d116681b0d2476f9
mGuv <mattmcg.gamedev@gmail.com>
2017-10-11 21:01:30 +0100

    Add comments to the Node class

--- Core/Solving/Node.cs
+++ Core/Solving/Node.cs
@@ -1,13 +1,42 @@
 ﻿namespace Core.Solving
 {
+    /// <summary>
+    /// Represents a Node in the search tree
+    /// </summary>
     public class Node
     {
+        /// <summary>
+        /// The Coordinate location of the Node
+        /// </summary>
         public readonly Coordinate Location;
+
+        /// <summary>
+        /// The total cost of expanding this node
+        /// </summary>
         public readonly int TotalCost;
+
+        /// <summary>
+        /// The cost to reach this node from the start
+        /// </summary>
         public readonly int PathCost;
+
+        /// <summary>
+        /// The estimated cost to reach the goal from this Node
+        /// </summary>
         public readonly int HeuristicCost;
+
+        /// <summary>
+        /// The node that was expanded to reach this Node
+        /// </summary>
         public readonly Node PreviousNode;
 
+        /// <summary>
+        /// Creates a new Node with the given information
+        /// </summary>
+        /// <param name="location">The location of the Node</param>
+        /// <param name="pathCost">The cost along the path to reach this Node</param>
+        /// <param name="heuristicCost">The estimated cost to the goal from this Node</param>
+        /// <param name="previousNode">The Node that was expanded to reach this Node</param>
         public Node(Coordinate location, int pathCost, int heuristicCost, Node previousNode)
         {
             this.Location = location;
